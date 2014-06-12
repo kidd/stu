8961d72598ed6c727469fd2e57037af1a92e25e2
Toni Reina <areina0@gmail.com>
2014-06-12 20:29:03 +0200

    Initial code for track_data_worker (totally useless right now)

--- brain/app/models/track.rb
+++ brain/app/models/track.rb
@@ -1,3 +1,9 @@
 class Track < ActiveRecord::Base
   has_many :scrobbles
+
+  after_create :collect_data
+
+  def collect_data
+    TrackDataWorker.perform_async(self.id)
+  end
 end
---
+++ brain/app/workers/track_data_worker.rb
@@ -0,0 +1,19 @@
+class TrackDataWorker
+  include Sidekiq::Worker
+
+  def perform(track_id)
+    track = Track.find(track_id)
+
+    uri = URI("https://7d05ce34-4a0166229897.my.apitools.com/recording/#{track.mbid}")
+    params = {
+      inc: "releases artist-credits tags"
+    }
+    uri.query = URI.encode_www_form(params)
+    res       = Net::HTTP.get_response(uri)
+    info = Hash.from_xml(res.body)["metadata"]
+
+    length = info["recording"]["length"]
+    artist = info["recording"]["artist_credit"]["name"]
+    genre  = info["recording"]["tag_list"]["tag"]["name"]
+  end
+end
