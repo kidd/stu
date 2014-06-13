94b3b692243a3523e5e41b144c6378bc33565167
Raimon Grau <raimonster@gmail.com>
2014-06-13 13:31:20 +0200

    get moar scrobbles

--- workers/lastfm.rb
+++ workers/lastfm.rb
@@ -4,15 +4,26 @@ require 'json'
 
 class Lastfm
 
+
+  # def self.get_results()
+
   # TODO: check that we got all the info of the timerange
   def self.scrobble(user, from, to, page = 1)
     response = HTTPClient.get("https://83f99d6a-4a0166229897.my.apitools.com/tracks?user=#{user}&limit=200")
     if response.ok?
-      tracks = JSON.parse(response.body)['recenttracks']['track']
+      body = JSON.parse(response.body)
+      tracks = body['recenttracks']['track']
       tracks.map!{|t| [t['date']['uts'], t['artist'], t['name'], t['mbid'] ] }
     end
 
-    tracks
+    attrs = body['recenttracks']['@attr']
+
+    if attrs['page'] < attrs['totalPages']
+      next_tracks, attrs = scrobble(user, from, to, page = page+1)
+      tracks =  tracks + next_tracks
+    end
+
+    return tracks, attrs
   end
 
 end
