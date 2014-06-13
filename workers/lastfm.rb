e96db0b5477e01f801e6ae1fbbf031e733c6d899
Raimon Grau <raimonster@gmail.com>
2014-06-13 12:53:53 +0200

    pick 200 songs

--- workers/lastfm.rb
+++ workers/lastfm.rb
@@ -5,12 +5,13 @@ require 'json'
 class Lastfm
 
   # TODO: check that we got all the info of the timerange
-  def self.scrobble(user, from, to)
-    response = HTTPClient.get("https://83f99d6a-4a0166229897.my.apitools.com/tracks?user=#{user}&from=#{from}")
+  def self.scrobble(user, from, to, page = 1)
+    response = HTTPClient.get("https://83f99d6a-4a0166229897.my.apitools.com/tracks?user=#{user}&limit=200")
     if response.ok?
       tracks = JSON.parse(response.body)['recenttracks']['track']
       tracks.map!{|t| [t['date']['uts'], t['artist'], t['name'], t['mbid'] ] }
     end
+
     tracks
   end
 
