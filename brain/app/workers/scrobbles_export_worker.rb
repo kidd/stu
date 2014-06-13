43e077048503d4ab625a06951f5d98089108f34a
Toni Reina <areina0@gmail.com>
2014-06-13 13:18:45 +0200

    Update scrobbles_export_worker to avoid problems with get params.

--- brain/app/workers/scrobbles_export_worker.rb
+++ brain/app/workers/scrobbles_export_worker.rb
@@ -7,27 +7,29 @@ class ScrobblesExportWorker
 
   def perform(scrobbles)
     table   = "stu"
-    inserts = []
 
-    scrobbles.each do |scrobble_id|
-      scrobble = Scrobble.find(scrobble_id)
-      columns  = ["userr", "track", "lon", "lat", "genre", "scrobbled_at"]
-      values   = [
-        "'#{scrobble.user}'",
-        "'#{scrobble.track.name}'",
-        scrobble.lon,
-        scrobble.lat,
-        "'#{scrobble.track.genre}'",
-        "'#{scrobble.scrobbled_at}'",
-      ]
+    scrobbles.each_slice(10) do |slice|
+      inserts = []
 
-      inserts << "INSERT INTO #{table} (#{columns.join(', ')}) VALUES (#{values.join(', ')})"
+      slice.each do |scrobble_id|
+        scrobble = Scrobble.find(scrobble_id)
+        columns  = ["userr", "track", "lon", "lat", "genre", "scrobbled_at"]
+        values   = [
+          "'#{scrobble.user}'",
+          "'#{scrobble.track.name}'",
+          scrobble.lon,
+          scrobble.lat,
+          "'#{scrobble.track.genre}'",
+          "'#{scrobble.scrobbled_at}'",
+        ]
+
+        inserts << "INSERT INTO #{table} (#{columns.join(', ')}) VALUES (#{values.join(', ')})"
+      end
+
+      uri    = URI("https://372c006e-4a0166229897.my.apitools.com/sql")
+      params = { q: inserts.join('; ') }
+      uri.query = URI.encode_www_form(params)
+      res   = Net::HTTP.get_response(uri)
     end
-
-    uri    = URI("https://372c006e-4a0166229897.my.apitools.com/sql")
-    params = { q: inserts.join('; ') }
-
-    uri.query = URI.encode_www_form(params)
-    res   = Net::HTTP.get_response(uri)
   end
 end
